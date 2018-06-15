11982269d593b91b6dbbd220f55421d79a49da8f
jaan-hou <houzhian@gmail.com>
2018-06-15 19:59:50 +0800

    remove author

--- paid_music_download.py
+++ paid_music_download.py
@@ -3,15 +3,9 @@
 # zhian.h@qq.com
 # python xxx.py
 
-# author: houzhian@gmail.com
-# python required, tested in python3,win10
-# warning: 支持正版，仅供测试
-# usage:
-
 import os
 import shutil
 
-# get cache_path from netease music config file
 cache_file_name=os.path.expanduser('~')+"/AppData/Local/Netease/CloudMusic/cache_path"
 buf=bytearray(os.path.getsize(cache_file_name))
 with open (cache_file_name,'rb') as f:
@@ -28,6 +22,7 @@ except Exception as e:
 print("clean cache folder finished")
 print("back to your netease music client and start to listen to the song")
 print("make sure that your songs has been fully cached")
+
 input("press ENTER if you are ready")
 
 if os.path.exists(cache_path)==False:
